759d8f27063441933f6de96cf9f32e49ab2167d8
Ben Ford <binford2k@overlookinfratech.com>
2026-01-22 12:59:53 -0800

    Update manifests/group.pp

--- manifests/group.pp
+++ manifests/group.pp
@@ -36,7 +36,7 @@ define yum::group (
       }
     }
     'latest': { # install the yum group and update if any packages are out of date.
-      exec { "yum-groupinstall-${name}":
+      exec { "yum-groupinstall-${name}-latest":
         command => join(concat(["yum -y group install '${name}'"], $install_options), ' '),
         unless  => "test $(yum --assumeno group install '${name}' 2>/dev/null| grep -c -e '^Install.*Package' -e '^Upgrade.*Package') -eq 0",
         timeout => $timeout,
